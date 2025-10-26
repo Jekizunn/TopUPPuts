7f6648ce7f0f281333049e24e08698eccba41095
Djaki <budonok82@gmail.com>
2025-10-26 13:18:56 +0700

    utsprototype1

--- lib/viewmodels/game_viewmodel.dart
+++ lib/viewmodels/game_viewmodel.dart
@@ -6,8 +6,8 @@ class GameViewModel {
         id: 1,
         title: "Call of Duty: Warzone",
         genre: "Battle Royale",
-        thumbnailUrl: "https://upload.wikimedia.org/wikipedia/en/6/65/COD_Warzone_Cover_Art.jpg", // Ganti URL
-        bannerUrl: "https://upload.wikimedia.org/wikipedia/en/6/65/COD_Warzone_Cover_Art.jpg", // Ganti URL
+        thumbnailUrl: "https://cdn.mobygames.com/covers/18400061-call-of-duty-warzone-mobile-iphone-front-cover.png", // Ganti URL
+        bannerUrl: "https://cdn.oneesports.id/cdn-data/sites/2/2024/03/COD-Warzone-Mobile.jpg", // Ganti URL
         category: "PC",
         platform: "PC, PS5, Xbox Series X/S", // Contoh
         publisher: "Activision", // Contoh
@@ -26,7 +26,7 @@ class GameViewModel {
         id: 2,
         title: "Apex Legends",
         genre: "Battle Royale",
-        thumbnailUrl: "https://via.placeholder.com/150/FF0000/FFFFFF?Text=Apex", // Ganti URL
+        thumbnailUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Apex_legends_simple_logo.jpg", // Ganti URL
         bannerUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1172470/f002ac48d98f501231e7d8bd3cc418b65c8d511a/capsule_616x353.jpg?t=1754578148", // Ganti URL
         category: "PC",
         isPopular: true,
@@ -46,7 +46,7 @@ class GameViewModel {
         id: 3,
         title: "Valorant",
         genre: "Tactical Shooter",
-        thumbnailUrl: "https://via.placeholder.com/150/FF4500/FFFFFF?Text=Valorant", // Ganti URL
+        thumbnailUrl: "https://pbs.twimg.com/profile_images/1247587680881434629/5e1tJWpf_400x400.jpg", // Ganti URL
         bannerUrl: "https://i0.wp.com/www.gimbot.com/wp-content/uploads/2022/11/VALORANT-Featured.jpg?fit=1200%2C675&ssl=1", // Ganti URL
         category: "PC",
         isPopular: true,
@@ -66,7 +66,7 @@ class GameViewModel {
         id: 4,
         title: "League of Legends: Wild Rift",
         genre: "MOBA",
-        thumbnailUrl: "https://via.placeholder.com/150/FFA500/000000?Text=LoLWR", // Ganti URL
+        thumbnailUrl: "https://i.pinimg.com/474x/0f/03/d7/0f03d71550ab2b51e63091b6af05befb.jpg", // Ganti URL
         bannerUrl: "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,f_auto,q_auto:best,w_640/v1571219351/qac2nnb942ezunfz0usr.jpg", // Ganti URL
         category: "Mobile",
         isPopular: true,
